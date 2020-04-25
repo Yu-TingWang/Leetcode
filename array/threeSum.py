f9586d29d58196ff8caf9db26a75ec99a94377e2
Yu-TingWang <yutingwang.wang@mail.utoronto.ca>
2020-04-25 16:27:13 -0400

    add threeSum.py

---
+++ array/threeSum.py
@@ -0,0 +1,60 @@
+"""
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique
+triplets in the array which gives the sum of zero .
+Restriction: The solution set must not contain duplicate triplets
+
+https://leetcode.com/problems/3sum/
+LeetCode No.15
+"""
+
+
+def threeSum_map(array: list, target):
+    array.sort()  # O(nlogn)
+    result = []
+    d = {}
+    # construct a dictionary that maps from value to its index
+    for i, num in enumerate(array):  d[num] = i
+    #  this is for checking the duplicate
+    check = set()
+
+    for i in range(len(array)):  # O(n)
+        for j in range(i, len(array)):  # O(n)
+            diff = target - array[i] - array[j]
+            if (diff in d) and (d[diff] > j) and ([i, j] not in check):  # O(1)
+                result.append((array[i], array[j], diff))
+                check.add([i, j])
+                result.append((array[i], array[j], d[diff]))
+    # the above loop takes O(n^2)
+    return result
+
+
+def threeSum_pointer(array: list, target):
+    array.sort()  # O(nlogn)
+    result = []
+    n = len(array)
+    for i in range(n - 2):  # O(n)
+        # The last two , if are one of ThreeSum, must be the array[r], so no need to take it as array[i]
+        if array[i] > target:  # then its impossible to find other two elements
+            break
+        if i > 0 and array[i] == array[i - 1]:  # adjust the index to the next diff number
+            continue
+        l = i + 1  # since [:i] has been tried
+        r = n - 1
+        while l < r:  # ~O(n)
+            total = array[i] + array[l] + array[r]
+            if total < target:  # need to make the sum bigger
+                l += 1
+            elif total > target:  # need to make the sum smaller
+                r -= 1
+            else:  # then this tuple is the threeSum
+                result.append([array[i], array[l], array[r]])
+                # adjust the left pointer to the next diff number
+                while l < r and array[l] == array[l + 1]:
+                    l += 1
+                # adjust the right pointer to the next diff number
+                while l < r and array[r] == array[r - 1]:
+                    r -= 1
+                l += 1
+                r -= 1
+        # the above loop takes O(n^2)
+    return result
